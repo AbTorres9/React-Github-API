0b4c98d915ffbce7619253f781829e81be7e141e
abhi.barthwal9@gmail.com <abhi.barthwal9@gmail,com>
2020-06-06 15:36:34 +0530

    logout and signin for user

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
           <Nav className="ml-auto" navbar>
             {context.user ? (
               <NavItem>
-                <NavLink tag={Link} to="/" className="text-white">
+                <NavLink onClick={() => {context.setUser(null)}} className="text-white">
                   Logout
                 </NavLink>
               </NavItem>
